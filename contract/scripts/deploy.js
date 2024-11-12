bf1094f6228c7eb1a100278929f8c9b8cab7450b
Danish <danish.eqbal2021@vitstudent.ac.in>
2024-11-12 22:44:10 +0530

    deployed smart contract

--- contract/scripts/deploy.js
+++ contract/scripts/deploy.js
@@ -1,20 +1,11 @@
-// scripts/deploy.js
 const { ethers } = require("hardhat");
 
 async function main() {
-    // Get the deployer's address (first signer)
     const [deployer] = await ethers.getSigners();
     console.log("Deploying contracts with the account:", deployer.address);
-
-    // Get the contract factory for the contract you want to deploy
     const Elections = await ethers.getContractFactory("Elections");
-
-    // Deploy the contract with constructor arguments
     const elections = await Elections.deploy();
-    
-    // Wait for the contract to be deployed
     await elections.deployed();
-
     console.log("Contract deployed to:", elections.address);
 }
 
